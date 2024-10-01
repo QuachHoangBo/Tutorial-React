042138f30ce7eee3197c3b76abfee9b983932cc4
Quach Hoang Bo <quachhoangbo2113@gmail.com>
2024-10-01 08:31:35 +0700

    video 17

--- LearnReactJS/Chapter2/src/components/MyComponent.js
+++ LearnReactJS/Chapter2/src/components/MyComponent.js
@@ -1,10 +1,20 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+  state = {
+    name: "Eric",
+    age: 26,
+    like: "girl",
+  };
+
   render() {
     return (
       <div>
         <h1>Hello World</h1>
+        <h1>
+          My name is {this.state.name} {this.state.age} years old and i like{" "}
+          {this.state.like}
+        </h1>
       </div>
     );
   }
