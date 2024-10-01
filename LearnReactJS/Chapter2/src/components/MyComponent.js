85beff940695ec2ced8a92088ef5636ec95402d3
Quach Hoang Bo <quachhoangbo2113@gmail.com>
2024-10-01 10:18:52 +0700

    video 20

--- LearnReactJS/Chapter2/src/components/MyComponent.js
+++ LearnReactJS/Chapter2/src/components/MyComponent.js
@@ -5,19 +5,33 @@ class MyComponent extends React.Component {
     name: "Eric",
     age: 26,
     like: "girl",
+    count: 0,
   };
+
+  CountNumber() {
+    this.setState({ count: Math.floor(Math.random() * 100) + 1 });
+  }
 
   render() {
     return (
-      <div>
+      <div className="container">
         <h1>Hello World</h1>
         <h1>
           My name is {this.state.name} {this.state.age} years old and i like{" "}
           {this.state.like}
         </h1>
+        <button
+          onClick={() => {
+            this.setState({ like: "boy" });
+          }}
+        >
+          click me
+        </button>
+        <button onClick={() => this.CountNumber()}>tăng số</button>
+        <h2>{this.state.count}</h2>
       </div>
     );
   }
 }
-
+<style>.container {}</style>;
 export default MyComponent;
