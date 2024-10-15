5339bd72174a96fc91690c82e00e6a0f80f927f0
Quach Hoang Bo <quachhoangbo2113@gmail.com>
2024-10-15 20:35:19 +0700

    video 47

--- LearnReactJS/Chapter2/src/App.js
+++ LearnReactJS/Chapter2/src/App.js
@@ -1,6 +1,7 @@
 // import logo from './logo.svg';
 import React from "react";
 import "./App.scss";
+import Header from "./components/Header/Header";
 // import MyComponent from "./components/MyComponent";
 // import { useDispatch, useSelector } from 'react-redux';
 // import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
@@ -9,7 +10,11 @@ const App = () => {
   // const count = useSelector(state => state.counter.count);
   // const dispatch = useDispatch();
 
-  return <div className="App">Hello my friend</div>;
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
 };
 
 export default App;
