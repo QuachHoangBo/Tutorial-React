44d4d1715f038556a03d3e31e16626d12823054b
Quach Hoang Bo <quachhoangbo2113@gmail.com>
2024-10-22 13:11:11 +0700

    vide 53.1 design header

--- LearnReactJS/Chapter2/src/components/Header/Header.js
+++ LearnReactJS/Chapter2/src/components/Header/Header.js
@@ -1,12 +1,15 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
+// import NavDropdown from "react-bootstrap/NavDropdown";
 import { NavLink } from "react-router-dom";
 
 function Header() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar
+      expand="lg"
+      style={{ backgroundColor: "rgba(255, 255, 255,0.5)", height: "80px" }}
+    >
       <Container>
         <NavLink to="/" className="navbar-brand">
           HomePage
@@ -25,7 +28,13 @@ function Header() {
             </NavLink>
           </Nav>
           <Nav>
-            <NavDropdown title="Settings" id="basic-nav-dropdown">
+            <button className="btn btn-outline-dark m-2" type="button">
+              Log in
+            </button>
+            <button className="btn btn-dark m-2" type="button">
+              Sign up
+            </button>
+            {/* <NavDropdown title="Settings" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
                 Another action
@@ -35,7 +44,7 @@ function Header() {
               <NavDropdown.Item href="#action/3.4">
                 Separated link
               </NavDropdown.Item>
-            </NavDropdown>
+            </NavDropdown> */}
           </Nav>
         </Navbar.Collapse>
       </Container>
--- LearnReactJS/Chapter2/src/components/Home/Home.js
+++ LearnReactJS/Chapter2/src/components/Home/Home.js
@@ -1,7 +1,23 @@
+import videoHomePage from "../assets/video-homepage.mp4";
+import "../Home/style.scss";
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="homepage-container">
+      <video className="homepage-video" autoPlay muted loop>
+        <source src={videoHomePage} />
+      </video>
+      <div className="homepage-content">
+        <div className="homepage-title1">There's a better way to ask</div>
+        <div className="homepage-title2">
+          <p>
+            You don't want to make a boring form. And your andience won't answer
+            one. Create a typeform instead-and make everyone happy.
+          </p>
+        </div>
+        <div className="homepage-button1">
+          <button>Get's started. It's free</button>
+        </div>
+      </div>
     </div>
   );
 };
